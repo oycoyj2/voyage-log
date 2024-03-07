35c46af5b131db227694d12657bc18835bb15232
oycoyj2 <oycoyj2@gmail.com>
2024-03-07 10:41:54 +0900

    first commit

---
+++ src/main/java/com/project/voyagelog/VoyageLogApplication.java
@@ -0,0 +1,13 @@
+package com.project.voyagelog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyageLogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoyageLogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/voyagelog/VoyageLogApplicationTests.java
@@ -0,0 +1,14 @@
+package com.project.voyagelog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageLogApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Hello World!");
+    }
+
+}
