de44434b91cf85467dd6e380c3e82a86648db566
oycoyj2 <oycoyj2@gmail.com>
2024-03-20 18:54:55 +0900

    착실하게 연습장으로 쓰는 중
    일지를 쓰는 규칙을  좀 명확히 해야하나? 아니면 그냥 지금처럼 자유롭게...?

--- src/main/java/com/project/voyagelog/VoyageLogApplication.java
+++ src/main/java/com/project/voyagelog/VoyageLogApplication.java
@@ -1,13 +1,36 @@
+/* 패키지 선언 */
 package com.project.voyagelog;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+/* 패키지 가져오기 */
+import java.lang.*;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
-@SpringBootApplication
-public class VoyageLogApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(VoyageLogApplication.class, args);
+class Art {
+    void display() {
+        System.out.println("아트 display 메서드");
     }
-
 }
+
+class Drawing extends Art {
+    void display() {
+        System.out.println("드로잉 display 메서드");
+    }
+}
+
+class Cartoon extends Drawing {
+}
+
+
+public class VoyageLogApplication {
+    public static void main(String[] args) {
+        Art art = new Art();
+        art.display();
+        Drawing drawing = new Drawing();
+        drawing.display();
+        Art art_drawing = new Drawing();
+        art_drawing.display();
+    }
+}
+
+
--- src/test/java/com/project/voyagelog/VoyageLogApplicationTests.java
+++ src/test/java/com/project/voyagelog/VoyageLogApplicationTests.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class VoyageLogApplicationTests {
 
+
     @Test
     void contextLoads() {
         System.out.println("Hello World!");
