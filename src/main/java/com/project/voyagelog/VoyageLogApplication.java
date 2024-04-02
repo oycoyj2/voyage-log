55094c1b00121d78765028cdbd464c601089e1c1
oycoyj2 <oycoyj2@gmail.com>
2024-04-02 11:08:45 +0900

    tomcat sevlet 라이브러리 추가
    본격적으로 스프링을 공부하기 시작했다. springboot를 사용하기 전 먼저 tomcat sevlet을 사용해 Containter가 어떤 역할을 하는지 알아보기로 한다.

--- src/main/java/com/project/voyagelog/VoyageLogApplication.java
+++ src/main/java/com/project/voyagelog/VoyageLogApplication.java
@@ -1,36 +1,16 @@
-/* 패키지 선언 */
 package com.project.voyagelog;
 
-/* 패키지 가져오기 */
-import java.lang.*;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 
-class Art {
-    void display() {
-        System.out.println("아트 display 메서드");
-    }
-}
-
-class Drawing extends Art {
-    void display() {
-        System.out.println("드로잉 display 메서드");
-    }
-}
-
-class Cartoon extends Drawing {
-}
-
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServer;
 
 public class VoyageLogApplication {
     public static void main(String[] args) {
-        Art art = new Art();
-        art.display();
-        Drawing drawing = new Drawing();
-        drawing.display();
-        Art art_drawing = new Drawing();
-        art_drawing.display();
+        TomcatServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
+        WebServer webServer = serverFactory.getWebServer();
+        webServer.start();
     }
+
 }
 
 
