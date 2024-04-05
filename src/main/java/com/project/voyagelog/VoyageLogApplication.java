94094dff506219616dd8765259710ea77c31478a
oycoyj2 <oycoyj2@gmail.com>
2024-04-05 18:56:20 +0900

    @MySpringBootApplication 변경
    바로 전에 커밋했던 것과 같은 내용

--- src/main/java/com/project/voyagelog/VoyageLogApplication.java
+++ src/main/java/com/project/voyagelog/VoyageLogApplication.java
@@ -1,34 +1,11 @@
 package com.project.voyagelog;
 
 
+import com.project.config.MySpringBootApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServer;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.context.support.GenericWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
 
-import java.io.IOException;
-
-@Configuration
-@ComponentScan
+@MySpringBootApplication
 public class VoyageLogApplication {
-    @Bean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
-    }
-
-    @Bean
-    public DispatcherServlet dispatcherServlet() {
-        return new DispatcherServlet();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(VoyageLogApplication.class, args);
 
