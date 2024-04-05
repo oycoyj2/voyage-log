c9f2851d9a059824e20690fca69e33da33669026
oycoyj2 <oycoyj2@gmail.com>
2024-04-05 10:45:56 +0900

    4월 5일 자 자바 항해
    오늘은 내부 클래스에 대해 정리했다. 사실 학습과 대부분의 정리는 어제 마무리했지만, 블로그 정리를 오늘 마무리하게 되어 커밋을 오늘에야 하게 되었다. 내부 클래스 -> 익명 클래스 -> 람다 함수를 학습하면 개념이 매끄럽게 연결될 것 같다.

--- src/main/java/com/project/InnerClassEx.java
+++ src/main/java/com/project/InnerClassEx.java
@@ -1,7 +1,8 @@
 package com.project;
-class Outer {
+class Liquid {
     int a = 1;
     String b = "Hello";
+    static Integer num = new Integer(9);
 
     class Inner {
         void printHello(String name) {
@@ -9,20 +10,80 @@ class Outer {
         }
     }
 
+    static private class Water {
+        String name;
+        int stock = 100;
+
+        Water() {
+            this("samdasoo");
+        }
+
+        Water(String name) {
+            this.name = name;
+        }
+    }
+
+    void speakStock() {
+        Water samdasoo = new Water();
+
+        System.out.println("어이 김씨 물이 몇 개 남았는가?");
+        System.out.println("아마 " + samdasoo.stock + "정도 있을게야");
+    }
+
 
 }
 
+class PocketBall {
+    int size = 100;
+    int price = 5000;
 
-public class InnerClassEx {
+    public void pocketMethod() {
+        int exp = 5000;
+
+        // 메서드 내에서 클래스를 선언
+        class PocketMonster {
+            String name = "이상해씨";
+            int level = 10;
+
+            public void getPoketLevel() {
+                System.out.println(level); // 인스턴스 변수 출력
+                System.out.println(exp); // 메서드 지역 상수 출력
+            }
+        }
+
+        // 메서드 내에서 클래스를 선언
+        class PocketMonster2 {
+            String name = "리자몽";
+            int level = 50;
+        }
+
+        new PocketMonster().getPoketLevel();
+        System.out.println("메서드 실행 완료");
+    }
+}
+
+interface Anonymous {
+    String amIAnonymous();
+}
+
+public class InnerClassEx implements Anonymous{
     public static void main(String[] args) {
-        Outer outer = new Outer();                  // 먼저 외부 클래스의 객체를 생성
-        Outer.Inner inner1 = outer.new Inner();     // 이후 내부 클래스 객체 생성
+        // Object 클래스를 일회성으로 익명 클래스로 선언하여 변수 o에 저장
+        Anonymous o = new Anonymous() {
+            @Override
+            public String amIAnonymous() {
+                return "아 제가 익명 클래스입니다.";
+            }
+        };    // 익명 클래스 뒤에는 반드시 세미콜론을 붙혀줘야 한다.
 
-        Outer.Inner inner2 = new Outer().new Inner();  //한 줄로 내부 클래스 생성하기
-
-        inner1.printHello(" InnerClass" + outer.a);
-//        inner2.printHello(" InnerClass" + inner2.a); // 내부 클래스에서 외부 클래스의 필드를 참조할 수 없음
+        // 익명 클래스의 객체의 오버라이딩한 메서드를 사용
+        String txt = o.amIAnonymous();
+        System.out.println(txt); // 아 제가 익명 클래스입니다.
     }
 
+    @Override
+    public String amIAnonymous() {
+        return null;
+    }
 }
 
